5c57ecf865959efec2964684c916ba1853230e07
Eug-VS <eug-vs@keemail.me>
2019-10-13 12:32:25 +0300

    Add fill() method, support size exception

--- src/algebra/Matrix.java
+++ src/algebra/Matrix.java
@@ -1,14 +1,28 @@
 package algebra;
 
+import java.util.Random;
+
 
 public class Matrix {
     private double[][] matrix;
+    private int m;
+    private int n;
 
-    public Matrix(int m, int n) {
-        matrix = new double[m][n];
-        for (double[] row : matrix) {
-            for (double item : row) {
-                item = 0;
+    public Matrix(int m, int n) throws ArrayIndexOutOfBoundsException {
+        this.matrix = new double[m][n];
+        if (m <= 0 || n <= 0) {
+            throw new ArrayIndexOutOfBoundsException("Matrix size <= 0");
+        }
+        this.m = m;
+        this.n = n;
+        this.fill();
+    }
+
+    private void fill() {
+        Random rand = new Random();
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                this.matrix[i][j] = rand.nextDouble();
             }
         }
     }
