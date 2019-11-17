f8fd38f81ec962a264d5f360b86f5d0c47c07e8b
Eug-VS <eug-vs@keemail.me>
2019-11-17 13:42:48 +0300

    Refactor code and use try-with-resources

--- src/Main.java
+++ src/Main.java
@@ -6,27 +6,26 @@ public class Main {
         final String inputPath = "input.txt";
         final String outputPath = "output.txt";
 
-        final BufferedReader reader = new BufferedReader(new FileReader(inputPath));
-        final BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath));
-
         final Pattern validSymbolsPattern= Pattern.compile("^[a-zA-Z\\d_]{8,}$");
         final Pattern containsCapitalLetter = Pattern.compile("^.*[A-Z].*$");
         final Pattern containsLowercaseLetter = Pattern.compile("^.*[a-z].*$");
         final Pattern containsDigitPattern = Pattern.compile("^.*\\d.*$");
 
-        String buffer;
-        while ((buffer = reader.readLine()) != null) {
-            final boolean match = (
-                validSymbolsPattern.matcher(buffer).matches() &&
-                containsCapitalLetter.matcher(buffer).matches() &&
-                containsLowercaseLetter.matcher(buffer).matches() &&
-                containsDigitPattern.matcher(buffer).matches()
-            );
-            writer.write(buffer + (match ? " - It's a match!" : ""));
-            writer.newLine();
+        try (
+            final BufferedReader reader = new BufferedReader(new FileReader(inputPath));
+            final BufferedWriter writer = new BufferedWriter(new FileWriter(outputPath))
+        ) {
+            String buffer;
+            while ((buffer = reader.readLine()) != null) {
+                final boolean match = (
+                    validSymbolsPattern.matcher(buffer).matches() &&
+                    containsCapitalLetter.matcher(buffer).matches() &&
+                    containsLowercaseLetter.matcher(buffer).matches() &&
+                    containsDigitPattern.matcher(buffer).matches()
+                );
+                writer.write(buffer + (match ? " - It's a match!" : ""));
+                writer.newLine();
+            }
         }
-
-        writer.close();
-        reader.close();
     }
 }
