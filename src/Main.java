86c284cc8654cbe2e25668309d246019f1ae19aa
Eug-VS <eug-vs@keemail.me>
2019-12-08 22:41:59 +0300

    Implement Formatter usage

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.Formatter;
 
 public class Main {
     public static void main(String[] args){
@@ -13,24 +14,32 @@ public class Main {
             input = args[0];
         }
 
-        final String defaultFormatString = "EEE MMM d HH:mm:ss zzz yyyy";
-        final String formatString = "yyyy MMMM, W 'week in a month,' a, m 'minutes' (z)";
+        final String defaultDateFormatString = "EEE MMM d HH:mm:ss zzz yyyy";
+        final String dateFormatString = "yyyy MMMM, W 'week in a month,' a, m 'minutes' (z)";
+        final String formatString = "%tA, %<tdth day of the month, %<tR %<Tp";
 
-        final SimpleDateFormat parser = new SimpleDateFormat(defaultFormatString);
-        final SimpleDateFormat formatter = new SimpleDateFormat(formatString);
+        final SimpleDateFormat dateParser = new SimpleDateFormat(defaultDateFormatString);
+        final SimpleDateFormat dateFormatter = new SimpleDateFormat(dateFormatString);
+        final Calendar calendar = Calendar.getInstance();
+        final Formatter formatter = new Formatter();
 
         try {
-            final Date date = parser.parse(input);
+            final Date date = dateParser.parse(input);
 
-            Calendar calendar = Calendar.getInstance();
             calendar.setTime(date);
 
             System.out.println("Day in year: " + calendar.get(Calendar.DAY_OF_YEAR));
             System.out.println("Day in month: " + calendar.get(Calendar.DAY_OF_MONTH));
             System.out.println("Day in week: " + calendar.get(Calendar.DAY_OF_WEEK));
-            System.out.println(formatter.format(date));
+
+            calendar.add(Calendar.DAY_OF_YEAR, 20);
+            System.out.println("DateTime in 20 days (Calendar): " + calendar.getTime());
+
+            System.out.println("SimpleDateFormat: " + dateFormatter.format(date));
+
+            System.out.println("Formatter: " + formatter.format(formatString, date));
         } catch (ParseException e) {
-            System.out.println("Can not parse using " + defaultFormatString);
+            System.out.println("Can not parse using " + defaultDateFormatString);
         }
     }
 }
