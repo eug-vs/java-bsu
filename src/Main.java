e23197a41710526b324a1de60b941b654e972818
Eug-VS <eug-vs@keemail.me>
2019-12-08 21:41:17 +0300

    Implement usage of SimpleDateFormat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args){
+        final String input = "Mon Dec 14 23:45:34 MSK 2017";
 
+        final String defaultFormatString = "EEE MMM d HH:mm:ss zzz yyyy";
+        final String formatString = "yyyy MMMM, W 'week in a month,' a, m 'minutes' (z)";
+
+        final SimpleDateFormat parser = new SimpleDateFormat(defaultFormatString);
+        final SimpleDateFormat formatter = new SimpleDateFormat(formatString);
+        try {
+            final Date date = parser.parse(input);
+            System.out.println("Formatted date using SimpleDateFormat: ");
+            System.out.println(formatter.format(date));
+        } catch (ParseException e) {
+            System.out.println("Can not parse using " + defaultFormatString);
+        }
     }
 }
