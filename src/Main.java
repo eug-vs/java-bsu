ee0c40810c475c3ed719dc26da21fb0cb1dc27be
Eug-VS <eug-vs@keemail.me>
2019-12-14 13:27:22 +0000

    Implement constructor based on recordString

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import core.MotorShip;
 
 public class Main {
     public static void main(String[] args){
-        final MotorShip testMotorShip = new MotorShip("Liberty", "4854856488");
+        final MotorShip testMotorShip = new MotorShip("Flying Dutchman, 54893214, 17.06.1790-17.06.1850");
         System.out.println(testMotorShip);
     }
 }
--- src/core/MotorShip.java
+++ src/core/MotorShip.java
@@ -1,5 +1,7 @@
 package core;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 
@@ -9,9 +11,29 @@ public class MotorShip {
     public Date releaseDate;
     public Date expirationDate;
 
-    public MotorShip(String name, String id) {
-        this.name = name;
-        this.id = id;
+    private final static String DELIMITERS = "-|, ";
+    private final static String DATE_FORMAT = "dd.MM.yyyy";
+    private final static SimpleDateFormat DATE_PARSER = new SimpleDateFormat(DATE_FORMAT);
+
+    private static Date parseDate(String dateString) {
+        try {
+            return DATE_PARSER.parse(dateString);
+        } catch (ParseException e) {
+            System.out.println("Exception occurred! Unable to parse date according to format " + DATE_FORMAT);
+            return new Date();
+        }
+    }
+
+    public MotorShip(String recordString) {
+        final String[] tokens = recordString.split(DELIMITERS);
+        try {
+            this.name = tokens[0];
+            this.id = tokens[1];
+            this.releaseDate = parseDate(tokens[2]);
+            this.expirationDate = parseDate(tokens[3]);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("Exception occurred! Record string doesn't provide enough information!");
+        }
     }
 
     public String toString() {
